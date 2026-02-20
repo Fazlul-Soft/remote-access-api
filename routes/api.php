83247d0577c153f817b59144033ecba5316a3a4d
Fazlul Hoque <sawravm299@gmail.com>
2026-02-20 15:48:00 +0600

    auto pair comment out

--- routes/api.php
+++ routes/api.php
@@ -17,7 +17,7 @@ Route::post('/login', [AuthController::class, 'login']);
 Route::post('/email/verify', [AuthController::class, 'verifyEmail']);
 Route::post('/email/resend', [AuthController::class, 'resendVerification']);
 
-Route::post('/devices/auto-pair', [DeviceController::class, 'autoPair']);
+// Route::post('/devices/auto-pair', [DeviceController::class, 'autoPair']);
 
 Route::middleware('auth:sanctum')->group(function () {
 
