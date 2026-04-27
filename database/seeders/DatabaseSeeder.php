db6433547d9f902ee633de91d91c5070bce46d38
Fazlul Hoque <sawravm299@gmail.com>
2026-04-27 14:38:14 +0600

    seeder add

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,6 +15,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $this->call(AdminSeeder::class);
+        $this->call(
+            [
+                AdminSeeder::class,
+                SubscriptionPlanSeeder::class
+            ]
+        );
     }
 }
